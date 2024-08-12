f49abd3aa9b4030626c10e0d7ad6f6f6fabe2e02
Oyundari23 <dariaa0560@gmail.com>
2024-08-12 15:27:01 +0800

    08/12 anigid

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,13 @@
 export function Footer() {
     return (
-        <div>
+        <div className="bg-slate-800 py-12 px-88 flex">
+            <div className="flex justify-between">
+                <div className="font-bold flex-1"> About </div>
+                <div className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
+                <div className="flex-col ">Home Blog Contact</div>
+                
 
+            </div>
         </div>
     )
 }
--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 // import { CiSearch } from "react-icons/ci";
 // import { IoIosSearch } from "react-icons/io";
+import { IoIosSearch } from "react-icons/io";
 
 const navs = [
     {
@@ -21,9 +22,9 @@ const navs = [
 
 export function Header() {
     return (
-        <div className="flex gap-5 ">
-           <div className="justify-between"> < Image src="/Images/Logo.png" width={158} height={36} className="bg-slate-50 rounded-xl py-2 px-2"/></div> 
-            <div className="flex gap-10">
+        <div className="flex gap-5 items-center mb-8">
+           <div className="flex-1"> < Image src="/Images/Logo.png" width={158} height={36} className="bg-slate-50 rounded-xl py-2 px-2"/></div> 
+            <div className="flex gap-[40px] flex-1">
                 {navs.map((item) => (
                     <div>
                         {item.link}
@@ -32,9 +33,9 @@ export function Header() {
                 
                 ))}
             </div>
-            <div className="bg-slate-600 w-60 px-4 h-8 rounded-xl ">
-                search
-                <div> </div>
+            <div className="bg-slate-600 w-60 h-8 px-4 py-1 h-8 rounded-lg flex items-center gap-10">
+                <div>search</div>
+                <div><IoIosSearch /> </div>
             </div>
                 {/* <Link href={"/Blog"} /> */}
 
